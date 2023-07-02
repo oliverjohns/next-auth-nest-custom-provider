6b9dad6921944aa0d2511047ad4d51f62e6a90d2
Oliver Johns <oliver.folke@protonmail.com>
2023-07-02 15:49:51 +0200

    BankID MVP

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Nextjs with next-auth connect with custom OIDC provider running in NestJs
 This is just a proof of concept of how to connect nextJs app with a OIDC provider in NestJS.
 
 Many thanks to this [https://github.com/adrianbrs/nest-oidc-provider](https://github.com/adrianbrs/nest-oidc-provider) repo for providing the OIDC implementation in Nestjs.
+
+# How to start
+
+1. `npm i && npm run dev` in `next-app` dir
+2. `yarn && npm run start:dev` in `nestjs-oidc-provider` dir
+3. Go to localhost:3000 and try logging in, it should take you to the nestjs one
--- nestjs-oidc-provider/src/app.module.ts
+++ nestjs-oidc-provider/src/app.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
-import { OidcModule } from 'nest-oidc-provider';
-import { OidcConfigModule } from './config/oidc-config.module';
-import { OidcConfigService } from './config/oidc-config.service';
-import { AppController } from './app.controller';
-import { InteractionModule } from './interaction/interaction.module';
+import { Module } from "@nestjs/common";
+import { OidcModule } from "nest-oidc-provider";
+import { OidcConfigModule } from "./config/oidc-config.module";
+import { OidcConfigService } from "./config/oidc-config.service";
+import { AppController } from "./app.controller";
+import { InteractionModule } from "./interaction/interaction.module";
+import { ScheduleModule } from "@nestjs/schedule";
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     OidcModule.forRootAsync({
       imports: [OidcConfigModule],
       useExisting: OidcConfigService,
