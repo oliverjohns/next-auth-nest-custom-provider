ee088f219793e21771cdb371dfa07366a9e97515
Oliver Johns <oliver.folke@protonmail.com>
2023-07-02 15:55:14 +0200

    frontend cleanup

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -1,13 +1,13 @@
-import Layout from "../components/layout"
+import Layout from "../components/layout";
 
 export default function IndexPage() {
   return (
     <Layout>
-      <h1>NextAuth.js Example</h1>
+      <h1>Example Next App</h1>
       <p>
-        This is an example site to demonstrate how to use{" "}
-        <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
+        This is an example Next app that connects to the NestJs BankID OIDC
+        provider via next-auth and gets a session from it.
       </p>
     </Layout>
-  )
+  );
 }
